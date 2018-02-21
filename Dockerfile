9c8106dd75659e36969c870dfa331b915f1a7db9
Stanislav Shandyga <ssh@anadeainc.com>
2018-02-21 17:49:43 +0200

    Add sample request response cycle

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,4 @@
 FROM golang
 WORKDIR /src
+EXPOSE 3000
 CMD ["go", "run", "main.go"]
--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("Test message.")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "{ \"message\": \"Test message\" }")
+	})
+
+	http.ListenAndServe(":3000", nil)
 }
